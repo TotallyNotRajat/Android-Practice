fcb9f9287b124c994f0483d3a0cb458e09fd45ca
Rajat Tiwari <rajattiwari32@gmail.com>
2025-09-17 15:00:30 +0530

    Added a counter

--- app/src/main/java/com/todo/week1/MainActivity.java
+++ app/src/main/java/com/todo/week1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.todo.week1;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,44 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+private int count = 0;
+    Button btnCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnCount = findViewById(R.id.btnCounter);
+        TextView txt = findViewById(R.id.txtName);
+        Button reset = findViewById(R.id.btnReset);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        btnCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            count++;
+            txt.setText(String.valueOf(count)); // convert number to string
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = 0;
+                txt.setText("Kya bakwas hai ye"); // convert number to string
+            }
+        });
+
     }
+
+
+
 }
